202ccd03aab4a384122e2f0c197d5b2c71ebbbb4
Subhadip Ghorui <subhadipghorui105@gmail.com>
2021-08-16 00:19:23 +0530

    Laravel Cache and Observer

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
+class PostController extends Controller
+{
+    public function index(){
+        $posts = Cache::remember('posts', 60*60, function () {
+            return Post::all();
+        });
+
+
+        return response($posts,200);
+    }
+    public function store(Request $request){
+        $post = Post::create($request->all());
+        return response($post,200);
+    }
+}
---
+++ app/Observers/PostObserver.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Cache;
+
+class PostObserver
+{
+    /**
+     * Handle the Post "created" event.
+     *
+     * @param  \App\Models\Post  $post
+     * @return void
+     */
+    public function created(Post $post)
+    {
+        Cache::forget('posts');
+    }
+
+    /**
+     * Handle the Post "updated" event.
+     *
+     * @param  \App\Models\Post  $post
+     * @return void
+     */
+    public function updated(Post $post)
+    {
+        Cache::forget('posts');
+    }
+
+    /**
+     * Handle the Post "deleted" event.
+     *
+     * @param  \App\Models\Post  $post
+     * @return void
+     */
+    public function deleted(Post $post)
+    {
+        Cache::forget('posts');
+    }
+
+    /**
+     * Handle the Post "restored" event.
+     *
+     * @param  \App\Models\Post  $post
+     * @return void
+     */
+    public function restored(Post $post)
+    {
+        Cache::forget('posts');
+    }
+
+    /**
+     * Handle the Post "force deleted" event.
+     *
+     * @param  \App\Models\Post  $post
+     * @return void
+     */
+    public function forceDeleted(Post $post)
+    {
+        Cache::forget('posts');
+    }
+}
